6e73d3742a997a9236c30b071cfa00b6e989a2c1
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-15 21:26:26 +0400

    If else switch case

--- BCTSO-20-NC-2/Lecture2/Program.cs
+++ BCTSO-20-NC-2/Lecture2/Program.cs
@@ -4,7 +4,88 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // არითმეტიკული + - * / %    ++ -- += -= *= /=
+            // ლოგიკური > < >= <= == !=
+            // შედარების || ან     && და
+            // მინიჭების =
+
+
+            //int number1 = 15;
+            //int number2 = 10;
+
+
+            //switch (number1)
+            //{
+            //    case int x when x > 1 && x < 4:
+            //        Console.WriteLine("AN ORI AN OTXTI");
+            //        break;
+            //    default:
+            //        Console.WriteLine("WADI TAVI DAMANEBE");
+            //        break;
+            //}
+
+
+            //if (number1 > 1 && number1 < 4 || number1 ==2 || number1 > 0)
+            //{
+            //    Console.WriteLine("AN ERTI An ORI AN SAMI");
+            //}
+            //else if (number1 == 2)
+            //{
+            //    Console.WriteLine("ORI");
+            //}
+            //else if (number1 == 3)
+            //{
+            //    Console.WriteLine("SAMI");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("WADI TAVI DAMANEBE");
+            //}
+
+
+            //ლოგიკური
+            //Console.WriteLine(number1 == number2);
+            //Console.WriteLine(number1 != number2);
+            //Console.WriteLine(number1 > number2); 
+            //Console.WriteLine(number1 < number2); 
+            //Console.WriteLine(number1 >= number2);
+            //Console.WriteLine(number1 <= number2);
+
+
+
+
+            //ბინარული
+            //Console.WriteLine(number1 + number2);//12
+            //Console.WriteLine(number1 - number2);//8
+            //Console.WriteLine(number1 * number2);//20
+            //Console.WriteLine(number1 / number2); //5
+            //Console.WriteLine(number1 % number2); //0
+
+
+            //არაბინარული
+            //Console.WriteLine(number1++);
+            ////Console.WriteLine(--number1);
+            //Console.WriteLine(number1 += 1);
+            //Console.WriteLine(number1 -= 1);
+            //Console.WriteLine(number1 *= 1);
+            //Console.WriteLine(number1 /= 1);
+
+
+            //if (number1 > number2)
+            //{
+            //    Console.WriteLine("METIA");
+            //}
+            //else if (number1 == number2)
+            //{
+            //    Console.WriteLine("METIA");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("NAKLEBIA");
+            //}
+
+
+
         }
     }
 }
