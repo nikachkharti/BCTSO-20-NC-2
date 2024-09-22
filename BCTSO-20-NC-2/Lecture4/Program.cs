3a4fbf5d84ad7803be9d715c597a6c6d259c9a59
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-22 21:56:10 +0400

    Arrays

---
+++ BCTSO-20-NC-2/Lecture4/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lecture4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbersArray = new int[3];
+            //numbersArray[0] = 10;
+            //numbersArray[1] = 12;
+            //numbersArray[2] = 3;
+
+            //int[] numbersArray = { 10, 12, 3 };
+
+            //Random random = new Random();
+
+            //for (int i = 0; i < numbersArray.Length; i++)
+            //{
+            //    numbersArray[i] = random.Next(1,101);
+            //}
+
+            //for (int i = 0; i < numbersArray.Length; i++)
+            //{
+            //    Console.WriteLine(numbersArray[i]);
+            //}
+
+
+
+            //int[][] secondCollection = new int[3][];
+            //secondCollection[0] = [10, 2, 4];
+            //secondCollection[1] = [3, 3, 5];
+            //secondCollection[2] = [3, 3, 1];
+
+            //int diagonalSum = 0;
+
+            //for (int i = 0; i < secondCollection.Length; i++)
+            //{
+            //    diagonalSum += secondCollection[i][i];
+            //}
+
+        }
+    }
+}
