0e77b672a9c7d58ec5bf139a42824def395ffc7c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-09-29 22:13:47 +0400

    Pracite

---
+++ BCTSO-20-NC-2/Lecture6/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Lecture6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = [10, 3, 5, 2, 10, 12, 66, 10];
+
+            //FindAll(10) => (10,10,10)
+
+        }
+
+
+
+
+
+
+
+        public static int[] Sort(int[] collection)
+        {
+            for (int i = 0; i < collection.Length - 1; i++)
+            {
+                for (int j = i + 1; j < collection.Length; j++)
+                {
+                    if (collection[j] < collection[i])
+                    {
+                        int temp = collection[j];
+                        collection[j] = collection[i];
+                        collection[i] = temp;
+                    }
+                }
+            }
+
+            return collection;
+        }
+
+
+        //არასწორი გზა ესე არ წეროთ!!!
+        //public static Tuple<int, int> CountEvenAndOddElements(int[] collection)
+        //{
+        //    int oddResult = 0;
+        //    int evenResult = 0;
+
+        //    for (int i = 0; i < collection.Length; i++)
+        //    {
+        //        if (collection[i] % 2 == 0)
+        //        {
+        //            evenResult++;
+        //        }
+        //        else
+        //        {
+        //            oddResult++;
+        //        }
+        //    }
+
+        //    return Tuple.Create(oddResult, evenResult);
+        //}
+
+        public static int CountEvenElements(int[] collection)
+        {
+            int result = 0;
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (collection[i] % 2 == 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+        public static int CountOddElements(int[] collection)
+        {
+            int result = 0;
+
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (collection[i] % 2 != 0)
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
