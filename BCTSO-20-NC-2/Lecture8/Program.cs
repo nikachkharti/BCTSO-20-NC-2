b26fe16580cdd91dd64b73435ce8985bad03535d
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-06 21:33:35 +0400

    INheritence, Polymorphyism

--- BCTSO-20-NC-2/Lecture8/Program.cs
+++ BCTSO-20-NC-2/Lecture8/Program.cs
@@ -1,12 +1,84 @@
 ﻿using Lecture8.MiniBank.Models;
 
-//1.მემკვიდრეობა
-//2.პოლიმორფიზმი
+//1.მემკვიდრეობა +
+//2.პოლიმორფიზმი (მრავალფორმიანობა) +
 //3.ენკაფსულაცია +
 //4.აბსტრაქცია
 
 
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
 
+    public Person(string firstName, string lastName, int age)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public Person()
+    {
+    }
+
+    public virtual void Talk() => Console.WriteLine($"Hello I am a Person {FirstName} {LastName} I am {Age} years old");
+}
+
+public class Employee : Person
+{
+    public double Salary { get; set; }
+
+    public Employee(string firstName, string lastName, int age, double salary) : base(firstName, lastName, age)
+    {
+        Salary = salary;
+    }
+
+    public Employee()
+    {
+    }
+
+    public override void Talk() => Console.WriteLine($"Hello I am a Employee {FirstName} {LastName} I am {Age} years old I have salary {Salary} GEL");
+}
+
+public class Student : Person
+{
+    public double Score { get; set; }
+    public Subject Subject { get; set; }
+
+    public Student(string firstName, string lastName, int age) : base(firstName, lastName, age)
+    {
+    }
+
+    public Student()
+    {
+    }
+
+    public override void Talk() => Console.WriteLine($"Hello I am a Student {FirstName} {LastName} I am {Age} years old I have subject {Subject.Title} and {Score} score");
+
+}
+
+
+public class Lecturer : Employee
+{
+    public Lecturer(string firstName, string lastName, int age, double salary) : base(firstName, lastName, age, salary)
+    {
+    }
+
+    public Lecturer()
+    {
+    }
+    public Subject Subject { get; set; }
+
+    public override void Talk() => Console.WriteLine($"Hello I am a Student {FirstName} {LastName} I am {Age} years old I have subject {Subject.Title} and salary {Salary} GEL");
+}
+
+
+public class Subject
+{
+    public string Title { get; set; }
+}
 
 
 
@@ -16,9 +88,46 @@ namespace Lecture8
     {
         static void Main()
         {
+            Person personObj = new Person()
+            {
+                FirstName = "Giorgi",
+                LastName = "Giorgadze",
+                Age = 18
+            };
+            personObj.Talk();
+
+            Employee employee = new Employee()
+            {
+                FirstName = "Giorgi",
+                LastName = "Menteshashvili",
+                Age = 20,
+                Salary = 1000
+            };
+            employee.Talk();
+
+            Student student = new Student()
+            {
+                FirstName = "Nikoloz",
+                LastName = "Kikiani",
+                Age = 29,
+                Score = 100,
+                Subject = new Subject() { Title = "C#" }
+            };
+            student.Talk();
+
+            Lecturer lecturer = new()
+            {
+                FirstName = "Nikoloz",
+                LastName = "Chkhartishvili",
+                Age = 29,
+                Salary = 100,
+                Subject = new Subject() { Title = "C#" }
+            };
+            lecturer.Talk();
 
 
 
+            //SayHello(lecturer);
 
 
             #region MINI BANK
@@ -59,5 +168,11 @@ namespace Lecture8
             //Console.WriteLine(client2.Account); 
             #endregion
         }
+
+
+        public static void SayHello(Person person) => Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+        //public static void SayHello(Employee person) => Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+        //public static void SayHello(Student person) => Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+        //public static void SayHello(Lecturer person) => Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
     }
 }
