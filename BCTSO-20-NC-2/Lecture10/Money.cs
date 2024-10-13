630c820c17f1e61dc5ac58db6f9fde409e92737b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-13 21:00:42 +0400

    Operator overload

---
+++ BCTSO-20-NC-2/Lecture10/Money.cs
@@ -0,0 +1,122 @@
+﻿namespace Lecture10
+{
+    public class Money
+    {
+        public string Currency { get; set; }
+        public decimal Amount { get; set; }
+
+        public Money(decimal amount, string currency)
+        {
+            Currency = currency;
+            Amount = amount;
+        }
+
+        public static Money operator +(Money first, Money second)
+        {
+            //if (first.Currency.Length == 3 && second.Currency.Length == 3 && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+            //{
+            //    return new Money(amount: first.Amount + second.Amount, currency: first.Currency);
+            //}
+            //return null;
+
+            //ტერნარული ოპერატორი   ?  :
+
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? new Money(amount: first.Amount + second.Amount, currency: first.Currency)
+                : null;
+        }
+        public static Money operator -(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? new Money(amount: first.Amount - second.Amount, currency: first.Currency)
+                : null;
+        }
+        public static Money operator *(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? new Money(amount: first.Amount * second.Amount, currency: first.Currency)
+                : null;
+        }
+        public static Money operator /(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? new Money(amount: first.Amount / second.Amount, currency: first.Currency)
+                : null;
+        }
+
+        public static bool operator >(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount > second.Amount
+                : false;
+        }
+        public static bool operator <(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount < second.Amount
+                : false;
+        }
+        public static bool operator <=(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount <= second.Amount
+                : false;
+        }
+        public static bool operator >=(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount >= second.Amount
+                : false;
+        }
+        public static bool operator ==(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount == second.Amount
+                : false;
+        }
+        public static bool operator !=(Money first, Money second)
+        {
+            return (first.Currency.Length == 3
+                && second.Currency.Length == 3
+                && first.Currency.Trim().ToUpper() == second.Currency.Trim().ToUpper())
+                ? first.Amount != second.Amount
+                : false;
+        }
+
+        public static Money operator ++(Money first)
+        {
+            //return new Money(amount: first.Amount += 1, currency: first.Currency);
+            first.Amount += 1;
+            return first;
+        }
+        public static Money operator --(Money first)
+        {
+            //return new Money(amount: first.Amount -= 1, currency: first.Currency);
+            first.Amount -= 1;
+            return first;
+        }
+
+
+        public override string ToString() => $"{Amount} {Currency}";
+        public override bool Equals(object obj) => base.Equals(obj);
+        public override int GetHashCode() => base.GetHashCode();
+    }
+}
---
+++ BCTSO-20-NC-2/Lecture10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lecture10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new(100, "GEL");
+            Money m2 = new(101, "GEL");
+
+
+        }
+
+
+
+    }
+}
