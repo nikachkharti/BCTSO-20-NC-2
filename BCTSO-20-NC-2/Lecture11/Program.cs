28887ef579ed8b89505464d5b233d865a86bf28b
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-17 20:26:34 +0400

    File read/write

---
+++ BCTSO-20-NC-2/Lecture11/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lecture11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ჩაწერა
+            //File.WriteAllText(@"../../../Test.txt", "Gamarjoba me var nika 2");
+            //File.AppendAllText(@"../../../Test.txt", "\nGamarjoba me var nika 2".Trim());
+            //File.AppendAllLines(@"../../../Test.txt", names);
+
+            //წაკითხვა
+            //string result = 
+            //    File.ReadAllText(@"../../../Test.txt");
+            //string[] result =
+            //File.ReadAllLines(@"../../../Test.txt");
+            //byte[] result = File.ReadAllBytes(@"../../../Test.txt");
+        }
+    }
+}
