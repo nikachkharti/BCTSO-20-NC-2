e842df9bb91c7e09df9e8219c436ac1c709b71fa
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-10-24 22:02:34 +0400

    Logging delegate

--- BCTSO-20-NC-2/Lecture13/Program.cs
+++ BCTSO-20-NC-2/Lecture13/Program.cs
@@ -1,26 +1,31 @@
 ﻿namespace Lecture13
 {
+    public delegate void LoggingDelegate(string message);
+
     internal class Program
     {
         static void Main(string[] args)
         {
             //დაწერეთ ლოგერი რომელიც არგუმენტად მიიღებს დელეგატს, და გადაცემული დელეგატის ფუნქციის მიხედვით
             //განისაზღვრება ლოგირება მოხდეს ფაილში თუ ჩაწერა მოხდეს კონსოლში.
+
+            Log("Hello World!", LogInConsole);
         }
 
-        public void Log(string message, LogInConsole)
+        public static void Log(string message, LoggingDelegate loggingDelegate)
         {
-            throw new NotImplementedException();
+            loggingDelegate(message);
         }
 
-        public static void LogInFile(string message)
-        {
-            throw new NotImplementedException();
-        }
 
-        public static void LogInConsole(string message)
+        // კონკრეტული ფუნქციები
+        private static void LogInFile(string message)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(@"../../../TestLogger.txt", message);
+        }
+        private static void LogInConsole(string message)
+        {
+            Console.Write(message);
         }
     }
 }
