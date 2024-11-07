3f5402b32651ed126ad87ffb3950862dabd0cbfc
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-07 20:53:27 +0400

    Classwork

---
+++ BCTSO-20-NC-2/MiniBank.Repository/CustomerCsvRepository.cs
@@ -0,0 +1,32 @@
+﻿using MiniBank.Models;
+
+namespace MiniBank.Repository
+{
+    public class CustomerCsvRepository
+    {
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
