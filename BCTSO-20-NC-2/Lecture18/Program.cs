057d7731e09f0cb673e5d2ac0427bc1c7a6a8e98
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-10 21:16:37 +0400

    Json

---
+++ BCTSO-20-NC-2/Lecture18/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Lecture18
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Hobbies { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+            {
+                new Person()
+                {
+                    Id = 1,
+                    Name = "Alice",
+                    Age = 30,
+                    Hobbies = new List<string>()
+                    {
+                        "Reading Boooks",
+                        "Playing football"
+                    }
+                },
+                new Person()
+                {
+                    Id = 2,
+                    Name = "Bob",
+                    Age = 16,
+                    Hobbies = new List<string>()
+                    {
+                        "Gaming",
+                        "Playing guitar"
+                    }
+                }
+            };
+
+            ////სერიალიზაცია Json ში
+            //string jsonString = JsonSerializer.Serialize(people, new JsonSerializerOptions()
+            //{
+            //    WriteIndented = true
+            //});
+
+            //Newtonsoft
+            //string jsonString = JsonConvert.SerializeObject(people, Formatting.Indented);
+            //File.WriteAllText(@"../../../PeopleAsJson.json", jsonString);
+
+
+            //დესერიალიზაცია
+            //string text = File.ReadAllText("../../../PeopleAsJson.json");
+            //var peopleList = JsonSerializer.Deserialize<List<Person>>(text);
+
+            //Newtonsoft
+            //var peopleList = JsonConvert.DeserializeObject<List<Person>>(text);
+
+        }
+    }
+}
