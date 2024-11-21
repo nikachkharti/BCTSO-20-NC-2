742af60c2cc7fc331e901a0c33f0c4fba99ea970
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-21 21:54:15 +0400

    attributes

---
+++ BCTSO-20-NC-2/Lecture21/CustomAttributes/EndDateGreaterThanStartDateAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lecture21.CustomAttributes
+{
+    public class EndDateGreaterThanStartDateAttribute : ValidationAttribute
+    {
+        private readonly string _startDatePropertyName;
+        public EndDateGreaterThanStartDateAttribute(string startDatePropertyName)
+        {
+            _startDatePropertyName = startDatePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            DateTime? endDate = (DateTime)value;
+            var startDateProperty = validationContext.ObjectType.GetProperty(_startDatePropertyName);
+
+            if (startDateProperty == null)
+            {
+                return new ValidationResult($"Unknown property: {_startDatePropertyName}");
+            }
+
+            DateTime? startDate = (DateTime?)startDateProperty.GetValue(validationContext.ObjectInstance);
+
+            if (startDate != null && endDate != null && startDate > endDate)
+            {
+                return new ValidationResult($"The {validationContext.DisplayName} must be greater to start date");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- BCTSO-20-NC-2/Lecture21/Program.cs
+++ BCTSO-20-NC-2/Lecture21/Program.cs
@@ -1,4 +1,8 @@
-﻿using MiniBank.Models;
+﻿using Lecture21.CustomAttributes;
+using MiniBank.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace Lecture21
@@ -7,6 +11,10 @@ namespace Lecture21
     {
         public string FirstName { get; set; }
 
+        public DateTime StartDate { get; set; }
+
+        [EndDateGreaterThanStartDate(nameof(StartDate))]
+        public DateTime EndDate { get; set; }
     }
 
     internal class Program
@@ -14,16 +22,26 @@ namespace Lecture21
         static void Main(string[] args)
         {
 
-            //Student std1 = new()
-            //{
-            //    FirstName = "Lana"
-            //};
+            Student std1 = new()
+            {
+                FirstName = "Lana",
+                StartDate = DateTime.Now,
+                EndDate = DateTime.Now.AddDays(1)
+            };
+
+
+            ValidationContext validationContext = new(std1);
+            List<ValidationResult> results = new();
+
+            bool isValid = Validator.TryValidateObject(std1, validationContext, results, true);
+
+
 
             //Type studentType = std1.GetType();
             //MethodInfo[] methods = studentType.GetMethods();
             //PropertyInfo[] properties = studentType.GetProperties();
 
-            DtoGenerator.GenerateDto(typeof(Account), "C:\\Users\\User\\Desktop\\IT STEP\\BCTSO-20-NC-2\\BCTSO-20-NC-2\\Lecture21\\Dtos\\");
+            //DtoGenerator.GenerateDto(typeof(Account), "C:\\Users\\User\\Desktop\\IT STEP\\BCTSO-20-NC-2\\BCTSO-20-NC-2\\Lecture21\\Dtos\\");
 
         }
 
