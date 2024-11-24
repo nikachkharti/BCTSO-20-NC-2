fc5c4661c6137875ed7804332061ee15b8b742e5
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2024-11-24 21:53:45 +0400

    Threads

---
+++ BCTSO-20-NC-2/Lecture22/Program.cs
@@ -0,0 +1,138 @@
+﻿//ნაკადი
+
+//Thread thread1 = new(() => TimerUp("Timer up"));
+//Thread thread2 = new(() => TimerDown("Timer down"));
+
+//thread1.Start();
+//thread2.Start();
+
+
+
+//void TimerDown(string timerName)
+//{
+//    for (int i = 10; i >= 1; i--)
+//    {
+//        Thread.Sleep(1000);
+//        Console.WriteLine($"{timerName}: {i}");
+//    }
+//}
+
+//void TimerUp(string timerName)
+//{
+//    for (int i = 1; i <= 10; i++)
+//    {
+//        Thread.Sleep(1000);
+//        Console.WriteLine($"{timerName}: {i}");
+//    }
+//}
+
+
+
+
+
+//int counter = 0;
+
+////.NET 8 -
+////object incrementLocker = new();
+
+////.NET 9 +
+//Lock incrementLocker = new();
+
+//Thread thread1 = new(IncrementCounter);
+//thread1.Name = "First Thread";
+
+//Thread thread2 = new(IncrementCounter);
+//thread2.Name = "Second Thread";
+
+//thread1.Start();
+//thread2.Start();
+
+//thread1.Join();
+//thread2.Join();
+
+//Console.WriteLine($"Final counter value is: {counter}");
+
+
+//void IncrementCounter()
+//{
+//    for (int i = 0; i < 100000; i++)
+//    {
+//        #region LOCK
+//        //lock (incrementLocker)
+//        //{
+//        //    int temp = counter;
+//        //    counter = temp + 1;
+//        //} 
+//        #endregion
+
+
+//        #region MONITOR
+
+//        //try
+//        //{
+//        //    Monitor.Enter(incrementLocker);
+//        //    int temp = counter;
+//        //    counter = temp + 1;
+//        //}
+//        //finally
+//        //{
+//        //    Monitor.Exit(incrementLocker);
+//        //}
+
+//        #endregion
+
+
+//        //MUTEX
+
+
+//        //SEMAPHORE
+//    }
+//}
+
+
+
+
+
+
+
+int counter = 0;
+
+ReaderWriterLockSlim incrementLocker = new ReaderWriterLockSlim();
+
+Thread thread1 = new(IncrementCounter);
+thread1.Name = "First Thread";
+
+Thread thread2 = new(IncrementCounter);
+thread2.Name = "Second Thread";
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine($"Final counter value is: {counter}");
+
+
+
+
+//READER WRITE LOCK
+void IncrementCounter()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+    #region READ/WRITE LOCK
+
+    try
+    {
+        incrementLocker.EnterWriteLock();
+        int temp = counter;
+        counter = temp + 1;
+    }
+    finally
+    {
+        incrementLocker.ExitWriteLock();
+    }
+    #endregion
+}
+}
--- BCTSO-20-NC-2/MiniBank.UI/Form1.cs
+++ BCTSO-20-NC-2/MiniBank.UI/Form1.cs
@@ -1,10 +1,28 @@
-namespace MiniBank.UI
+﻿namespace MiniBank.UI
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void msgBtn1_Click(object sender, EventArgs e)
+        {
+            Thread t1 = new(() => ChangeLabelText("მესიჯი 1", 2000));
+            t1.Start();
+        }
+
+        private void msgBtn2_Click(object sender, EventArgs e)
+        {
+            Thread t2 = new(() => ChangeLabelText("მესიჯი 2", 3000));
+            t2.Start();
+        }
+
+        private void ChangeLabelText(string message, int delay)
+        {
+            Thread.Sleep(delay);
+            msgLabel.Text = message;
         }
     }
 }
