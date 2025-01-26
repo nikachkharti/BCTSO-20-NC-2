920d83b00061905cae03daef68f1543fece15e6c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-01-26 20:39:25 +0400

    Operations

---
+++ BCTSO-20-NC-2/MiniBank.Repository/SqlClientOperationRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniBank.Repository
+{
+    public class SqlClientOperationRepository
+    {
+        private const string _connectionString = "Server=DESKTOP-SCSHELD\\SQLEXPRESS;Database=MiniBankBCTSO20N;Trusted_Connection=true;TrustServerCertificate=true";
+
+        Task Insert(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task Withdraw(int accountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task Transfer(int sourceAccountId, int destinationAccountId, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
