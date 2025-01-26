6efdcca916264393aee3857fd1c354ba038f133e
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-01-26 21:16:26 +0400

    Operations implementation

--- BCTSO-20-NC-2/MiniBank.Repository/SqlClientOperationRepository.cs
+++ BCTSO-20-NC-2/MiniBank.Repository/SqlClientOperationRepository.cs
@@ -1,22 +1,143 @@
-﻿namespace MiniBank.Repository
+﻿using Microsoft.Data.SqlClient;
+using MiniBank.Models;
+
+namespace MiniBank.Repository
 {
     public class SqlClientOperationRepository
     {
         private const string _connectionString = "Server=DESKTOP-SCSHELD\\SQLEXPRESS;Database=MiniBankBCTSO20N;Trusted_Connection=true;TrustServerCertificate=true";
+        private SqlClientAccountRepository sqlClientAccountRepository = new();
 
-        Task Insert(int accountId, decimal amount)
+        public async Task Insert(int accountId, decimal amount)
         {
-            throw new NotImplementedException();
+            if (amount <= 0 || accountId <= 0)
+            {
+                throw new ArgumentException("Invalid arguments passed");
+            }
+
+            var account = await sqlClientAccountRepository.GetAccount(accountId);
+
+            if (account is null)
+            {
+                throw new NullReferenceException("Account not found");
+            }
+
+            account.Balance += amount;
+
+            await sqlClientAccountRepository.Update(account);
+
+
+            await Create(new Operation()
+            {
+                OperationType = OperationType.Credit,
+                Currency = account.Currency,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                AccountId = account.Id
+            });
+        }
+        public async Task Withdraw(int accountId, decimal amount)
+        {
+            if (amount <= 0 || accountId <= 0)
+            {
+                throw new ArgumentException("Invalid arguments passed");
+            }
+
+            var account = await sqlClientAccountRepository.GetAccount(accountId);
+
+            if (account is null)
+            {
+                throw new NullReferenceException("Account not found");
+            }
+
+            if (account.Balance >= amount)
+            {
+                account.Balance -= amount;
+            }
+            else
+            {
+                throw new ArgumentException("Insufficient funds to withdraw");
+            }
+
+            await sqlClientAccountRepository.Update(account);
+            await Create(new Operation()
+            {
+                OperationType = OperationType.Credit,
+                Currency = account.Currency,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                AccountId = account.Id
+            });
+        }
+        public async Task Transfer(int sourceAccountId, int destinationAccountId, decimal amount)
+        {
+            if (amount <= 0 || sourceAccountId <= 0 || destinationAccountId <= 0)
+            {
+                throw new ArgumentException("Invalid arguments passed");
+            }
+
+            var sourceAccount = await sqlClientAccountRepository.GetAccount(sourceAccountId);
+            var destinationAccount = await sqlClientAccountRepository.GetAccount(destinationAccountId);
+
+            if (sourceAccount is null || destinationAccount is null)
+            {
+                throw new NullReferenceException("Account not found");
+            }
+
+            if (sourceAccount.Balance >= amount)
+            {
+                sourceAccount.Balance -= amount;
+                destinationAccount.Balance += amount;
+            }
+            else
+            {
+                throw new ArgumentException("Insufficient funds to withdraw");
+            }
+
+            await sqlClientAccountRepository.Update(sourceAccount);
+            await sqlClientAccountRepository.Update(destinationAccount);
+
+            await Create(new Operation()
+            {
+                OperationType = OperationType.Debit,
+                Currency = sourceAccount.Currency,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                AccountId = sourceAccount.Id
+            });
+
+            await Create(new Operation()
+            {
+                OperationType = OperationType.Credit,
+                Currency = destinationAccount.Currency,
+                Amount = amount,
+                HappendAt = DateTime.Now,
+                AccountId = destinationAccount.Id
+            });
         }
 
-        Task Withdraw(int accountId, decimal amount)
+        public async Task Create(Operation operation)
         {
-            throw new NotImplementedException();
+            string commandText = "spCreateOperation";
+
+            using (SqlConnection connection = new(_connectionString))
+            {
+                using (SqlCommand command = new(commandText, connection))
+                {
+                    command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    command.Parameters.AddWithValue("operationType", operation.OperationType);
+                    command.Parameters.AddWithValue("currency", operation.Currency);
+                    command.Parameters.AddWithValue("amount", operation.Amount);
+                    command.Parameters.AddWithValue("happendAt", operation.HappendAt);
+                    command.Parameters.AddWithValue("accountId", operation.AccountId);
+
+                    await connection.OpenAsync();
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
         }
 
-        Task Transfer(int sourceAccountId, int destinationAccountId, decimal amount)
-        {
-            throw new NotImplementedException();
-        }
+
     }
 }
