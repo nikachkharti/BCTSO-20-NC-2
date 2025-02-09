e5ac63492713af1b326b470934904c595671ef79
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-09 21:29:05 +0400

    Migrations

---
+++ BCTSO-20-NC-2/University.Models/Entities/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models.Entities
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        [Column(TypeName = "CHAR(11)")]
+        public string PersonalNumber { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        [EmailAddress]
+        [Column(TypeName = "VARCHAR(50)")]
+        public string Email { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
