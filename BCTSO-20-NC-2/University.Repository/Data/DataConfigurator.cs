940a8d59916fddd20f32f2c1dc3876c5a71bdae9
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-02-16 20:40:04 +0400

    Fluent API configuration

--- BCTSO-20-NC-2/University.Repository/Data/DataConfigurator.cs
+++ BCTSO-20-NC-2/University.Repository/Data/DataConfigurator.cs
@@ -4,8 +4,153 @@ using University.Models.Entities;
 
 namespace University.Repository.Data
 {
-    public static class DataSeeder
+    public static class DataConfigurator
     {
+        public static void ConfigureStudents(this ModelBuilder modelBuilder)
+        {
+            //Student Entity
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(s => s.Id);
+                entity
+                .Property(s => s.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+                entity
+                .Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity
+                .Property(s => s.PersonalNumber)
+                .IsRequired()
+                .HasColumnType("CHAR(11)")
+                .HasMaxLength(11);
+
+                entity
+                .Property(s => s.Email)
+                .IsRequired()
+                .HasColumnType("VARCHAR(50)")
+                .HasMaxLength(50);
+
+                entity
+                .HasOne(s => s.Address)
+                .WithOne(a => a.Student)
+                .HasForeignKey<Address>(a => a.StudentId);
+
+                entity
+                .HasMany(s => s.Groups)
+                .WithOne(g => g.Student)
+                .HasForeignKey(g => g.StudentId);
+            });
+        }
+        public static void ConfigureAddresses(this ModelBuilder modelBuilder)
+        {
+            //Address Entity
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.HasKey(a => a.Id);
+                entity
+                .Property(a => a.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+                entity
+                .Property(a => a.City)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity
+                .Property(a => a.Street)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity
+                .HasOne(a => a.Student)
+                .WithOne(s => s.Address)
+                .HasForeignKey<Address>(a => a.StudentId);
+            });
+        }
+        public static void ConfigureTeachers(this ModelBuilder modelBuilder)
+        {
+            //Teacher Entity
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.HasKey(t => t.Id);
+                entity
+                .Property(t => t.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+                entity
+                .Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity
+                .HasMany(t => t.Courses)
+                .WithOne(c => c.Teacher)
+                .HasForeignKey(c => c.TeacherId);
+
+            });
+        }
+        public static void ConfigureCourses(this ModelBuilder modelBuilder)
+        {
+            //Course Entity
+            modelBuilder.Entity<Course>(entity =>
+            {
+                entity.HasKey(c => c.Id);
+                entity
+                .Property(c => c.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+                entity
+                .Property(c => c.Title)
+                .IsRequired()
+                .HasMaxLength(150);
+
+                entity
+                .HasOne(c => c.Teacher)
+                .WithMany(t => t.Courses)
+                .HasForeignKey(c => c.TeacherId);
+
+                entity
+                .HasMany(c => c.Groups)
+                .WithOne(g => g.Course)
+                .HasForeignKey(g => g.CourseId);
+            });
+        }
+        public static void ConfigureGroups(this ModelBuilder modelBuilder)
+        {
+            //Group Entity
+            modelBuilder.Entity<Group>(entity =>
+            {
+                entity.HasKey(g => g.Id);
+                entity
+                .Property(g => g.Id)
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+                entity
+                .Property(g => g.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+
+                entity
+                .HasOne(g => g.Student)
+                .WithMany(s => s.Groups)
+                .HasForeignKey(g => g.StudentId);
+
+                entity
+                .HasOne(g => g.Course)
+                .WithMany(c => c.Groups)
+                .HasForeignKey(g => g.CourseId);
+            });
+        }
+
+
         public static void SeedStudents(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Student>().HasData
@@ -36,7 +181,6 @@ namespace University.Repository.Data
                     }
                 );
         }
-
         public static void SeedAddresses(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Address>().HasData
@@ -64,7 +208,6 @@ namespace University.Repository.Data
                     }
                 );
         }
-
         public static void SeedTeachers(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Teacher>().HasData
@@ -81,7 +224,6 @@ namespace University.Repository.Data
                     }
                 );
         }
-
         public static void SeedCourses(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Course>().HasData
@@ -106,7 +248,6 @@ namespace University.Repository.Data
                     }
                 );
         }
-
         public static void SeedGroups(this ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Group>().HasData(
