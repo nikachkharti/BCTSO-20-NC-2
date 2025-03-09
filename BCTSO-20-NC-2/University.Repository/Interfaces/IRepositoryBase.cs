05ecc5938b7202b78f4e55e22b4641ecb4491601
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-03-09 21:01:52 +0400

    .

--- BCTSO-20-NC-2/University.Repository/Interfaces/IRepositoryBase.cs
+++ BCTSO-20-NC-2/University.Repository/Interfaces/IRepositoryBase.cs
@@ -4,7 +4,10 @@ namespace University.Repository.Interfaces
 {
     public interface IRepositoryBase<T> where T : class
     {
+        ///TODO: დაამატეთ Paging
         Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter, string includeProperties = null);
+
+        ///TODO: დაამატეთ Paging
         Task<List<T>> GetAllAsync(string includeProperties = null);
         Task<T> GetAsync(Expression<Func<T, bool>> filter, string includeProperties = null);
         Task AddAsync(T entity);
